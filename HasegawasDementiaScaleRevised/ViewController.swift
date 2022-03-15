1c600d63e90a805f528948d4e4f4a7d09f4e699e
muranakar <P1211065@gmail.com>
2022-03-15 09:12:34 +0900

    Initial Commit

---
+++ HasegawasDementiaScaleRevised/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HasegawasDementiaScaleRevised
+//
+//  Created by 村中令 on 2022/03/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

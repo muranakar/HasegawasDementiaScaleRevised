a8566eed99ae71f8a0fe4d3f3ec345e2c5d81b10
muranakar <P1211065@gmail.com>
2022-03-19 05:55:08 +0900

    Assessmentファイル、StoryBoard追加

---
+++ HasegawasDementiaScaleRevised/Assessment/AssessmentViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AssessmentViewController.swift
+//  HasegawasDementiaScaleRevised
+//
+//  Created by 村中令 on 2022/03/19.
+//
+
+import UIKit
+
+class AssessmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}

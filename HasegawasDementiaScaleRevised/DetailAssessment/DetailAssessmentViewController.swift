d06c9120bbe419d4384b7958cc47c425d8e9abe5
muranakar <P1211065@gmail.com>
2022-03-19 16:58:25 +0900

    Detailファイルの作成。StoryBoard未完成。

---
+++ HasegawasDementiaScaleRevised/DetailAssessment/DetailAssessmentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailAssessmentViewController.swift
+//  HasegawasDementiaScaleRevised
+//
+//  Created by 村中令 on 2022/03/19.
+//
+
+import UIKit
+
+class DetailAssessmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

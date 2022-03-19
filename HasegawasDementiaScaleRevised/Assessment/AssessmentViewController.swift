494b5cf8c7ce9337d29e7388612b9ebea9e2edc3
muranakar <P1211065@gmail.com>
2022-03-19 16:10:25 +0900

    Assessmentのデータ反映完了。ボタンのisHidden。Label。TextViewの変更完了。

--- HasegawasDementiaScaleRevised/Assessment/AssessmentViewController.swift
+++ HasegawasDementiaScaleRevised/Assessment/AssessmentViewController.swift
@@ -13,6 +13,9 @@ class AssessmentViewController: UIViewController {
 
     @IBOutlet private weak var progressTextLabel: UILabel!
     @IBOutlet private weak var progressView: UIProgressView!
+    @IBOutlet private weak var assessmentItemTitleLabel: UILabel!
+    @IBOutlet private weak var assessmentQuestionLabel: UILabel!
+    @IBOutlet private weak var assessmentAttentionTextView: UITextView!
     @IBOutlet private weak var button1: UIButton!
     @IBOutlet private weak var button2: UIButton!
     @IBOutlet private weak var button3: UIButton!
@@ -21,38 +24,107 @@ class AssessmentViewController: UIViewController {
     @IBOutlet private weak var button6: UIButton!
     @IBOutlet private weak var button7: UIButton!
 
-    var hdsrIndex: Int = 0
-
     required init?(coder: NSCoder, targetPerson: TargetPerson, repository: AssessmentRepository) {
         self.targetPerson = targetPerson
         self.repository = repository
         super.init(coder: coder)
     }
-
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    // JSONファイルから、デコードされた構造体を、配列で管理。
+    private var hdsrAssessment: [HDSRAssessment] = []
+    private var assessmentResultHDSR: [Int] = []
+    private var hdsrIndex: Int = 0
     // ボタンの配列
     private var buttons: [UIButton] {
         [
             button1, button2, button3, button4, button5, button6, button7
         ]
     }
-    // JSONファイルから、デコードされた構造体を、配列で管理。
-    private var hdsrAssessment: [HDSRAssessment] = []
+    private var hdsrButtonText: [String?] {
+        [
+            hdsrAssessment[hdsrIndex].button1Text,
+            hdsrAssessment[hdsrIndex].button2Text,
+            hdsrAssessment[hdsrIndex].button3Text,
+            hdsrAssessment[hdsrIndex].button4Text,
+            hdsrAssessment[hdsrIndex].button5Text,
+            hdsrAssessment[hdsrIndex].button6Text,
+            hdsrAssessment[hdsrIndex].button7Text
+        ]
+    }
+    private var hdsrResultNum: [Int?] {
+        [
+            hdsrAssessment[hdsrIndex].button1ResultNumber,
+            hdsrAssessment[hdsrIndex].button2ResultNumber,
+            hdsrAssessment[hdsrIndex].button3ResultNumber,
+            hdsrAssessment[hdsrIndex].button4ResultNumber,
+            hdsrAssessment[hdsrIndex].button5ResultNumber,
+            hdsrAssessment[hdsrIndex].button6ResultNumber,
+            hdsrAssessment[hdsrIndex].button7ResultNumber
+        ]
+    }
+
+    private var dictonaryButtonAndButtonText: [UIButton: String?] {
+        [UIButton: String?](uniqueKeysWithValues: zip(buttons, hdsrButtonText))
+    }
+
+    private var dictonaryButtonAndButtonResultNum: [UIButton: Int?] {
+        [UIButton: Int?](uniqueKeysWithValues: zip(buttons, hdsrResultNum))
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         decodeHDSRJsonFile()
-        print(hdsrAssessment)
+        // 以下、Viewに関するメソッド
+        buttonViewConfigue()
+        labelViewConfigue()
+        textViewViewConfigue()
+    }
 
+    @IBAction private func decide(sender: UIButton) {
+        assessmentResultHDSR.append(dictonaryButtonAndButtonResultNum[sender]!!)
+        print(assessmentResultHDSR)
+        hdsrIndex += 1
+        if hdsrIndex == 9 {
+            // TODO 画面遷移処理
+            let assessmet = Assessment(resultHDSR: .init(
+                itemAge: assessmentResultHDSR[0],
+                itemDateOrientation: assessmentResultHDSR[1],
+                itemPlaceOrientation: assessmentResultHDSR[2],
+                itemMemory: assessmentResultHDSR[3],
+                itemCalculation: assessmentResultHDSR[4],
+                itemDigitSpan: assessmentResultHDSR[5],
+                itemDelayedPlayback: assessmentResultHDSR[6],
+                itemVisualMemory: assessmentResultHDSR[7],
+                itemWordRecall: assessmentResultHDSR[8])
+            )
+            repository.add(value: assessmet, id: targetPerson.id)
+
+        } else {
+            buttonViewConfigue()
+            labelViewConfigue()
+            textViewViewConfigue()
+        }
     }
 }
 
-
-
-
+// MARK: - Viewに関する変更
+private extension AssessmentViewController {
+    func labelViewConfigue() {
+        assessmentItemTitleLabel.text = hdsrAssessment[hdsrIndex].itemName
+        assessmentQuestionLabel.text = hdsrAssessment[hdsrIndex].itemQuestion
+    }
+    func textViewViewConfigue() {
+        assessmentAttentionTextView.text = hdsrAssessment[hdsrIndex].itemAttention
+    }
+    func buttonViewConfigue() {
+    buttons.forEach { button in
+                button.isHidden = dictonaryButtonAndButtonText[button]! == nil
+                button.setTitle(dictonaryButtonAndButtonText[button]!, for: .normal)
+            }
+    }
+}
 
 // MARK: - JSONファイルのデコーダー
 private extension AssessmentViewController {

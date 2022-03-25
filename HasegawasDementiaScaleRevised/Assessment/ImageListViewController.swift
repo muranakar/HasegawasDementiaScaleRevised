c7cf67f0144b10e23e791f07d691fd44da4d5f86
muranakar <P1211065@gmail.com>
2022-03-26 08:16:09 +0900

    image画像の貼り付け。ImageList設定

---
+++ HasegawasDementiaScaleRevised/Assessment/ImageListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ImageListViewController.swift
+//  HasegawasDementiaScaleRevised
+//
+//  Created by 村中令 on 2022/03/26.
+//
+
+import UIKit
+
+class ImageListViewController: UIViewController {
+
+    @IBOutlet private weak var image1: UIImageView!
+    @IBOutlet private weak var image2: UIImageView!
+    @IBOutlet private weak var image3: UIImageView!
+    @IBOutlet private weak var image4: UIImageView!
+    @IBOutlet private weak var image5: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
